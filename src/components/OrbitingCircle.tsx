9a8a7b3fb8948ab0a65dd5905c96034773879c62
Himangshu Mishra <115614942+himangshumishra@users.noreply.github.com>
2024-07-04 18:28:25 +0530

    minor fixes

--- src/components/OrbitingCircle.tsx
+++ src/components/OrbitingCircle.tsx
@@ -42,9 +42,9 @@ export function OrbitingCircle() {
         <Icons.gitHub />
       </OrbitingCircles>
       <div className=" p-4 relative z-10 w-full text-center">
-<h1 className=" mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">We’ve really sped up our workflow
+<h1 className=" mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent text-blue-500 bg-gradient-to-b dark:from-neutral-50 to-neutral-400">We’ve really sped up our workflow
 </h1>
-<p className=" mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+<p className=" mt-4 font-normal text-base md:text-lg text-black max-w-lg mx-auto dark:text-neutral-300">
 We’ve just released a new update!Check out the all new dashboard view. Pages and now load faster.you can try us for free for 30 days.
 Join 4,000+ companies already growing
 </p>
